82f1e2fba9efaaf76ed030feb5c9fb04494cbfef
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-05-25 17:30:25 -0300

    fix: import fonts & set wrapper paddingHorizontal

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   SafeAreaView,
+  View,
   Text,
   Image,
   TouchableOpacity,
@@ -11,22 +12,26 @@ import { Feather } from '@expo/vector-icons';
 
 import wateringImg from '../assets/watering.png';
 
+import fonts from '../styles/fonts';
+
 import colors from '../styles/colors';
 
 export function Welcome() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>
-        Manage {'\n'} your plants {'\n'} even easier 🌻
-      </Text>
-      <Image source={wateringImg} style={styles.image} resizeMode="contain" />
-      <Text style={styles.subtitle}>
-        Don't forget to water your plants anymore. We take care to remind you
-        whenever you need
-      </Text>
-      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
-        <Feather name="chevron-right" style={styles.buttonIcon} />
-      </TouchableOpacity>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Manage {'\n'} your plants even {'\n'} more easier
+        </Text>
+        <Image source={wateringImg} style={styles.image} resizeMode="contain" />
+        <Text style={styles.subtitle}>
+          Don't forget to water your plants anymore. We take care to remind you
+          whenever you need
+        </Text>
+        <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+          <Feather name="chevron-right" style={styles.buttonIcon} />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
@@ -34,17 +39,24 @@ export function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  wrapper: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'space-around',
+    paddingHorizontal: 20,
   },
   title: {
-    fontSize: 32,
+    fontFamily: fonts.heading,
+    fontSize: 28,
     fontWeight: 'bold',
+    lineHeight: 34,
     textAlign: 'center',
     color: colors.heading,
     marginTop: 38,
   },
   subtitle: {
+    fontFamily: fonts.text,
     fontSize: 18,
     textAlign: 'center',
     color: colors.heading,
